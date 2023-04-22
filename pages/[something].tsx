661e65bf60d70b40325df8a6515621a435698768
mt2m10 <masashi_69@live.jp>
2023-04-22 09:29:56 +0900

    [add] プライバシーポリシーを追加

---
+++ pages/[something].tsx
@@ -0,0 +1,51 @@
+import fs, { promises } from "fs";
+import matter from "gray-matter";
+import { GetStaticPropsContext, InferGetStaticPropsType, NextPage } from "next";
+import path from "path";
+import markdownToHtml from "../lib/markdownToHtml";
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const Home: NextPage<Props> = ({ content }) => {
+  return (
+    <main className="container">
+      <article
+        dangerouslySetInnerHTML={{
+          __html: `${content}`,
+        }}
+      />
+    </main>
+  );
+};
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const postsDirectory = path.join(process.cwd(), "something");
+  const fileNames = await promises.readdir(postsDirectory);
+  const paths = fileNames.map((fileName) => {
+    return { params: { something: `${fileName.split(".")[0]}` } };
+  });
+  return {
+    paths: paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (
+  context: GetStaticPropsContext<{ something: string }>
+) => {
+  const contentPath = path.join(
+    process.cwd(),
+    `something/${context.params?.something}.md`
+  );
+  const fileContents = fs.readFileSync(contentPath, "utf-8");
+  const { content } = matter(fileContents);
+
+  return {
+    props: {
+      content: await markdownToHtml(content),
+    },
+  };
+};
+
+export default Home;
