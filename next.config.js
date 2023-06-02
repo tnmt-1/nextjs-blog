c9f7929014cb011b375a5c91b18cef8a806415cb
mt2m10 <masashi_69@live.jp>
2023-06-02 17:10:14 +0900

    runtimeを設定

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
     },
     experimental: {
         serverComponentsExternalPackages: ["@remark-embedder", "remark-prism"],
+        experimental: { runtime: 'edge' }
     },
 };
 
