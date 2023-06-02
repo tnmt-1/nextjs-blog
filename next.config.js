6f4c5efe95bb4f00d3a0008f411c2940684f7ce9
mt2m10 <masashi_69@live.jp>
2023-06-02 17:06:55 +0900

    ビルド方法を変更

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
     experimental: {
         serverComponentsExternalPackages: ["@remark-embedder", "remark-prism"],
     },
-    output: "export",
 };
 
 module.exports = nextConfig;
--- sitemap.config.js
+++ sitemap.config.js
@@ -2,7 +2,7 @@ module.exports = {
     siteUrl: 'https://blog.mah.blue',
     generateRobotsTxt: true,
     sitemapSize: 7000,
-    outDir: "./out",
+    outDir: "./.vercel/output/static",
     exclude: [
         "/site/*"
     ]
