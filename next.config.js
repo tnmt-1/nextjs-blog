7a0799beebadcf98a026f455107b8c4015d702fb
mt2m10 <masashi_69@live.jp>
2023-06-02 15:31:58 +0900

    exportできるようにした

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
     experimental: {
         serverComponentsExternalPackages: ["@remark-embedder", "remark-prism"],
     },
+    output: "export",
 };
 
 module.exports = nextConfig;
